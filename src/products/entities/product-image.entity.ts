8908acf62c1dd23b0e873e1d76ed3005e4cefc85
Alex AR <niteravilaalexis@gmail.com>
2024-10-04 15:31:59 -0500

    Cambio de nombre de las tablas

--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -1,7 +1,9 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Product } from ".";
 
-@Entity()
+@Entity({
+  name: 'products_images'
+})
 export class ProductImage {
 
   @PrimaryGeneratedColumn()
