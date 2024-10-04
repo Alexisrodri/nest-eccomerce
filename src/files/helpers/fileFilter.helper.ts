c308a374ba5dbccd3a8141fa367e694e89876e1a
Alex AR <niteravilaalexis@gmail.com>
2024-10-04 16:36:09 -0500

    fileFilter img for mimetype

---
+++ src/files/helpers/fileFilter.helper.ts
@@ -0,0 +1,12 @@
+
+
+export const fileFilter = (req: Express.Request, file: Express.Multer.File, callback: Function) => {
+  console.log({ file });
+  if (!file) return callback(new Error(`File is Empty`), false)
+  const fileExtension = file.mimetype.split('/')[1];
+  const validExtension = ['jpg', 'jpeg', 'png'];
+  if (validExtension.includes(fileExtension)) {
+    return callback(null, true)
+  }
+  callback(null, true)
+}
