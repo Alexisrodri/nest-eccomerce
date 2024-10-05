c40cae3bcf8db89051dcf6ec4890e41bb9e76950
Alex AR <niteravilaalexis@gmail.com>
2024-10-04 21:52:24 -0500

    SecureUrl

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,12 +1,26 @@
-import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { fileNamer, fileFilter } from './helpers';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) { }
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService
+  ) { }
+
+  @Get('/product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string
+  ) {
+    const path = this.filesService.getProductImage(imageName);
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {
@@ -22,7 +36,8 @@ export class FilesController {
     if (!file) {
       throw new BadRequestException(`Make sure that file is an image`)
     }
-    return file.originalname;
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`
+    return { secureUrl };
   }
 
 }
