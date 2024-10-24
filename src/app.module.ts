505e7e08271ab466db1dee907970ddb57c72f013
Alex AR <niteravilaalexis@gmail.com>
2024-10-24 12:52:39 -0500

    ports

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { AuthModule } from './auth/auth.module';
       },
       type: 'postgres',
       // host: process.env.DB_HOST,
-      // port: +process.env.DB_PORT,
+      port: +process.env.DB_PORT,
       // database: process.env.DB_NAME,
       // username: process.env.DB_USERNAME,
       // password: process.env.DB_PASSWORD,
